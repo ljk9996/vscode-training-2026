7f5cfce75b9bc9cd53186d970b2b93fd438a6c38
甲第 <772356634@qq.com>
2026-05-25 15:36:32 +0800

    更新 todo.py 调用参数

--- todo.py
+++ todo.py
@@ -1,20 +1,13 @@
-from utils import load_tasks, save_tasks, add_task, list_tasks, mark_done, delete_task
-
+from utils import load_tasks, save_tasks, add_task, list_tasks, mark_done
 
 def main():
     tasks = load_tasks()
-    add_task(tasks, "买牛奶")
-    add_task(tasks, "交电费")
-    add_task(tasks, "写周报")
+    add_task("买牛奶")
+    add_task("交电费")
+    add_task("写周报")
     list_tasks(tasks)
-
-    # 测试删除功能
-    delete_task(tasks, 2)
+    mark_done(tasks, 2)
     list_tasks(tasks)
-
-    mark_done(tasks, 1)
-    list_tasks(tasks)
-
 
 if __name__ == "__main__":
     main()
